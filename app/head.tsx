b139eb143b51a47d155f68c0d116eb0bc0846f31
hasanasadov <hasanaliasadov@gmail.com>
2025-07-17 12:13:58 +0400

    sitemap added

---
+++ app/head.tsx
@@ -0,0 +1,17 @@
+export default function Head() {
+  return (
+    <>
+      <title>Hasanali Asadov Portfolio</title>
+      <meta
+        name="google-site-verification"
+        content="google-verification-code"
+      />
+      <meta name="description" content="Personal Website" />
+      <link rel="canonical" href="https://asadov.site" />
+      <meta property="og:title" content="Hasanali Asadov Portfolio" />
+      <meta property="og:description" content="Hasanali Portfolio" />
+      <meta property="og:url" content="https://asadov.site" />
+      <meta property="og:type" content="website" />
+    </>
+  );
+}
--- app/metadata.ts
+++ app/metadata.ts
@@ -3,5 +3,20 @@ export const metadata = {
   description: "Next.js developer portfolio saytı",
   keywords: ["Hasanali Asadov", "portfolio", "Next.js", "developer"],
   authors: [{ name: "Hasanali Asadov" }],
+
+  openGraph: {
+    title: "Hasanali Asadov Portfolio",
+    description: "Developer portfolio saytı",
+    url: "https://asadov.site",
+    type: "website",
+    images: [
+      {
+        url: "https://asadov.site/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Hasanali Asadov Portfolio",
+      },
+    ],
+  },
   // sosial media, favicon, open graph və s. metadata da əlavə oluna bilər
 };
